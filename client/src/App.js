85dd03d3c8ee0cb0b990b1e2509490743f37f4f6
brajeshjaishwal <brajesh.jaishwal@gmail.com>
2018-09-07 12:41:37 +0530

    client side modifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Blogger.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route>
+          <Switch>
+            <Route />
+          </Switch>
+        </Route>
       </div>
     );
   }
