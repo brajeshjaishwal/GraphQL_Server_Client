55bd00608ea252c0a35ac5d6db118d06294b057f
brajeshjaishwal <brajesh.jaishwal@gmail.com>
2018-09-07 18:45:13 +0530

    minor modifications

--- db/models/post.js
+++ db/models/post.js
@@ -16,14 +16,13 @@ PostSchema.methods.doVote = async function (userId, like) {
     var result = await Votes.doVote({ userId, postId: post._id, like} )
     if(!result.alreadyVoted){
         post.vote.push(userId)
-        await post.save()
     }
-    else{
-        if(result.message !== undefined){
+    else if(result.message !== undefined){
             return result.message
-        }
-        return result.vote
     }
+    post.likes += result.vote
+    await post.save()
+    return post
 }
 
 var Post = mongoose.model('Post', PostSchema)
